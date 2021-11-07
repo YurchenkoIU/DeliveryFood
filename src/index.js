b4ba898be5ef438edbe3b61eb8693cbd762396a0
YurchenkoIU <92322290+YurchenkoIU@users.noreply.github.com>
2021-11-08 01:31:46 +0300

    lesson-3

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import auth from "./modules/auth.js";
-import partners from "./modules/partners.js";
-
-auth();
-partners();
--- src/menu.js
+++
@@ -1,5 +0,0 @@
-import auth from "./modules/auth.js";
-import menu from "./modules/menu.js";
-
-auth();
-menu();
